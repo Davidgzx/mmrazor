9ac9d9022c603a07019d4233b8696463742cab9e
zixuan.guo <zixuan.guo@qq.com>
2022-10-03 04:28:57 +1100

    update loss

--- mmrazor/models/losses/reviewkd_loss.py
+++ mmrazor/models/losses/reviewkd_loss.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from typing import List
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,28 +21,26 @@ class HCLLoss(nn.Module):
     def __init__(
         self,
         loss_weight: float = 1.0,
+        pool_sizes: List[int] = [4, 2, 1],
     ) -> None:
         super().__init__()
         self.loss_weight = loss_weight
+        self.pool_sizes = pool_sizes
 
     def forward(
         self,
         s_feature: torch.Tensor,
         t_feature: torch.Tensor,
     ) -> torch.Tensor:
-        loss_all = 0.0
         _, _, h, _ = s_feature.shape
         loss = F.mse_loss(s_feature, t_feature, reduction='mean')
-        cnt = 1.0
-        tot = 1.0
-        for size in [4, 2, 1]:
+        weight = 1.0
+        for size in self.pool_sizes:
             if size >= h:
                 continue
             pooled_fs = F.adaptive_avg_pool2d(s_feature, (size, size))
             pooled_ft = F.adaptive_avg_pool2d(t_feature, (size, size))
-            cnt /= 2.0
-            loss += F.mse_loss(pooled_fs, pooled_ft, reduction='mean') * cnt
-            tot += cnt
-        loss = loss / tot
-        loss_all += loss
-        return loss_all
+            weight /= 2.0
+            loss += F.mse_loss(pooled_fs, pooled_ft, reduction='mean') * weight
+        loss = loss / len(self.pool_sizes)
+        return loss
